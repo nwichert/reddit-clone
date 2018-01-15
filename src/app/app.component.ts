4b1543c763b2acfd79e3ff48b23221804f59c0bf
Nick Wichert <nick.wichert@icloud.com>
2018-01-15 17:20:14 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Article } from './article';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article(
+        'The Angular 2 screencast',
+        'The easiest way to learn Angular 2!',
+      ),
+      new Article(
+        'The Angular 3 screencast',
+        'The easiest way to learn Angular 2!'
+      ),
+      new Article(
+        'The Angular 4 screencast',
+        'The easiest way to learn Angular 2!'
+      ),
+      new Article(
+        'The Angular 5 screencast',
+        'The easiest way to learn Angular 2!'
+      )
+   ];
+  }
 }
